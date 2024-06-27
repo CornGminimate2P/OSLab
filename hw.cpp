2f4d3602a80284541de2fe016436a6e6d5c60016
CornGminimate2P <ftluconcrete@gmail.com>
2024-06-28 03:11:53 +0700

    kk

---
+++ hw.cpp
@@ -0,0 +1,115 @@
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <sys/types.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <wait.h>
+#include <sys/shm.h>
+#include <sys/stat.h>
+#include <sys/ipc.h>
+
+struct sharedmem
+{
+	float	nb[100];
+}	s;
+
+int     ft_str_is_alpha(char *str)
+{
+        int     i;
+
+        i = 0;
+        while (str[i])
+        {
+                if ((str[i] >= 'A') && (str[i] <= 'Z'))
+                        i++;
+                else if ((str[i] >= 'a') && (str[i] <= 'z'))
+                        i++;
+                else
+                        return (0);
+        }
+        return (1);
+}
+
+int	main(void)
+{
+	pid_t	pid_b;
+	pid_t	pid_c;
+	int	i;
+	char	ch[20];
+	//sharedmem	*s;
+
+	i = 0;
+	//ch = 0;
+	while (1)
+	{
+		printf("\nEnter value : ");
+		//scanf("%f", (float *)&s.nb[i]);
+		scanf("%s", ch);
+		if (!ft_str_is_alpha(ch))
+		{
+			s.nb[i] = atof(ch);
+			if (s.nb[i] <= 100)
+			{
+				i++;
+				pid_b = fork();
+				if (pid_b < 0)
+				{
+					fprintf(stderr, "Fork failed.\n");
+					exit(-1);
+				}
+				else if (pid_b == 0)
+				{
+					float	power;
+					int	n;
+
+					power = 0;
+					n = 0;
+					while (s.nb[n])
+					{
+						power = s.nb[n] * s.nb[n];
+						n++;
+						printf("Pow nb %d : %f\n", n, power);
+						sleep(1);
+					}
+					exit(0);
+				}
+				waitpid(pid_b, NULL, 0);
+				pid_c = fork();
+				if (pid_c < 0)
+				{
+					fprintf(stderr, "Fork failed.\n");
+					exit(-1);
+				}
+				else if (pid_c == 0)
+				{
+					float	sum;
+					int	n;
+
+					sum = 0;
+					n = 0;
+					while (s.nb[n])
+					{
+						sum += s.nb[n];
+						n++;
+						printf("Sum nb %d : %f\n", n, sum);
+						sleep(2);
+					}
+					exit(0);
+				}
+				waitpid(pid_c, NULL, 0);
+			}
+			else 
+			{
+				exit(0);
+			}
+		}
+		else
+		{
+			exit(-1);
+		}
+	}
+	exit(0);
+}	
