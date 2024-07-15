6355be074abccb9703e88741cbed69e9ec854e79
CornGminimate2P <ftluconcrete@gmail.com>
2024-07-15 17:46:05 +0700

    Week5naja

---
+++ week5/lab5_1edt.cpp
@@ -0,0 +1,122 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
+
+#define USE_MUTEX
+pthread_mutex_t mutex;
+
+float	data[100];
+float	power[100];
+float 	sum[100];
+int		i = 0;
+
+void *threadA(void *selector);
+void *threadB(void *selector);
+void *threadC(void *selector);
+
+int main(void)
+{
+	pthread_t	tid1, tid2, tid3;
+	pthread_attr_t	attr1, attr2, attr3;
+
+	pthread_attr_init(&attr1);
+	pthread_attr_init(&attr2);
+	pthread_attr_init(&attr3);
+
+	pthread_create(&tid1, &attr1, threadA, NULL);
+	pthread_create(&tid2, &attr2, threadB, NULL);
+	pthread_create(&tid3, &attr3, threadC, NULL);
+
+	pthread_join(tid1, NULL);
+	pthread_join(tid2, NULL);
+	pthread_join(tid3, NULL);
+
+	return 0;
+}
+
+void	*threadA(void *selector)
+{
+	float	numbani;
+
+	numbani = 0;
+	while(1)
+	{
+		printf("\nEnter value : ");
+		scanf("%f", &numbani);
+		pthread_mutex_lock(&mutex);
+		data[i] = numbani;
+		if (data[i] <= 100)
+		{
+			power[i] = data[i] * data[i];
+			if (i == 0)
+				sum[i] = data[i];
+			else
+				sum[i] = data[i] + sum[i-1];
+			i++;
+		}
+		else 
+		{
+			break;
+		}
+		pthread_mutex_unlock(&mutex);
+	}
+	pthread_exit(0);
+}
+
+void	*threadB(void *selector)
+{
+	int j = 0;
+
+	pthread_mutex_lock(&mutex);
+	while (data[j] <= 100)
+	{
+		pthread_mutex_unlock(&mutex);
+		if (i == 0)
+		{
+			printf("\nProcess B is wating for the first number\n");
+			sleep(2);
+		}
+		if (j == i)
+		{
+			j = 0;
+		}
+		else 
+		{
+			printf("\nPower of %d number is : %f\n", (j+1), power[j]);
+			j++;
+			sleep(1);
+		}
+		pthread_mutex_lock(&mutex);
+    }
+	pthread_mutex_unlock(&mutex);
+    pthread_exit(0);
+}
+
+void	*threadC(void *selector)
+{
+	int	j = 0;
+
+	pthread_mutex_lock(&mutex);
+	while (data[j] <= 100)
+	{
+		pthread_mutex_unlock(&mutex);
+		if (i == 0){
+			printf("\nProcess C is wating for the first number\n");
+			sleep(2);
+		}
+		if (j == i)
+		{
+			j = 0;
+		}
+		else
+		{
+			printf("\nTotal sum of %d number is : %f\n", (j+1), sum[j]);
+			j++;
+			sleep(2);
+		}
+		pthread_mutex_lock(&mutex);
+	}
+	pthread_mutex_unlock(&mutex);
+	pthread_exit(0);
+}
